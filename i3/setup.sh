c6ac5af0103131b6bb66e7f6c3fff9c760a6aca6
Paul Nykiel <paul.nykiel@gmail.com>
2020-01-21 22:49:35 +0100

    Added rofi to i3 config

--- i3/setup.sh
+++ i3/setup.sh
@@ -1,3 +1,5 @@
 cat config >> ~/.config/i3/config
 mkdir -p ~/.config/rofi/
 ln -s $(pwd)/rofi ~/.config/rofi/config
+sed -i "s/dmenu_rum/rofi -show combi -config .config/rofi/config/g" ~/.config/i3/config
+

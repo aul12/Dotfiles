b1ac6056b314d9ed170b44fe2390787dec8e6b4f
Paul Nykiel <paul.nykiel@gmail.com>
2020-01-22 20:48:40 +0100

    i3 poweroff shortcut

--- i3/setup.sh
+++ i3/setup.sh
@@ -2,4 +2,5 @@ cat config >> ~/.config/i3/config
 mkdir -p ~/.config/rofi/
 ln -s $(pwd)/rofi ~/.config/rofi/config
 sed -i "s/dmenu_rum/rofi -show combi -config .config/rofi/config/g" ~/.config/i3/config
-
+xdg-desktop-menu install aul12-reboot.desktop
+xdg-desktop-menu install aul12-poweroff.desktop

37284192f6a5e9117451c3b9e001597072902cbb
Paul Nykiel <paul.nykiel@gmail.com>
2020-01-22 11:19:05 +0100

    Fixed batter

--- cron/setup.sh
+++ cron/setup.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.cron
 ln -s $(pwd)/wallpaper.sh ~/.cron/wallpaper.sh
-ln -s $(pwd)/batterStatus.sh ~/.cron/batteryStatus.sh
+ln -s $(pwd)/batteryStatus.sh ~/.cron/batteryStatus.sh
 crontab -l > current_cron
 cat cron_add >> current_cron
 crontab current_cron

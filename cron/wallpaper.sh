14f0bc205b4197f34060f11950a6c5aa45144ca6
Paul Nykiel <paul.nykiel@gmail.com>
2020-02-19 11:19:50 +0100

    Small fixes

--- cron/wallpaper.sh
+++ cron/wallpaper.sh
@@ -2,5 +2,4 @@
 export DISPLAY=:0
 files=(/home/paul/Pictures/Südl\ Afrika\ 2018/* /home/paul/Pictures/Südl\ Afrika\ 2017/*)
 WALLPAPER=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-cp "$WALLPAPER" /home/paul/.cron/wallpaper.png
 /usr/bin/feh --bg-fill "$WALLPAPER"
